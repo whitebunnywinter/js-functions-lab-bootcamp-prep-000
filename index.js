a3c8e3943e3dfe06a55d2968d8145a393e752913
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-19 09:43:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
  return `Happy ${holiday}, ${name}!` 
 }
 
-function holidayCountdown(days, holoday) {
+function holidayCountdown(days, holiday) {
   return `It's ${days} days until ${holiday}!` 
 }
